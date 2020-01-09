1f531bcff3a1585ed3c5342c7c07e44fce947fcf
Paul Stølen <paul@dagbladet.no>
2020-01-09 22:43:36 +0100

    logruskimpy
    - Skimpy logrus output for when you're outputting to console and want to actually read the log message

---
+++ logruskimpy/logruskimpy.go
@@ -0,0 +1,22 @@
+package logruskimpy
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+// New - returns a logrus.Formatter that focuses on the log message
+// and data, by minimizing the function and file information
+func New() (lf logrus.Formatter) {
+	return &logrus.TextFormatter{
+		FullTimestamp:    false,
+		CallerPrettyfier: skimpyCaller,
+	}
+}
+
+func skimpyCaller(rf *runtime.Frame) (function string, file string) {
+	return "", fmt.Sprintf("%s:%d", filepath.Base(rf.File), rf.Line)
+}
